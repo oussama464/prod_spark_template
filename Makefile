4508a01198250c1e12e63f9bab7626cf64d390ea
oussama464 <oussama.missaoui201@gmail.com>
2022-07-31 15:53:57 +0200

    add tree in readme and refactor config.ini

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@ all:test build
 test:
 	cd tests && pytest
 build:
-	rm -rf ./dist && rm -rf libs/* && mkdir ./dist 
-	pip install -r requirements.txt -t ./libs 
-	cp ./main.py ./dist 
-	cp ./config.ini ./dist 
-	zip -r dist/jobs.zip jobs 
-	zip -r dist/shared.zip shared 
-	zip -r -D dist/libs.zip libs
+	rm -rf ./dist && rm -rf ./src/libs/* && mkdir ./dist 
+	pip install -r requirements.txt -t ./src/libs 
+	cp ./src/main.py ./dist 
+	cp ./src/config.ini ./dist 
+	zip -r dist/jobs.zip ./src/jobs 
+	zip -r dist/shared.zip ./src/shared 
+	zip -r -D dist/libs.zip ./src/libs
 setup_build:
 	rm -rf dist build && python setup.py sdist bdist_wheel && unzip -l dist/*.whl && tar --list -f dist/*.tar.gz
 build_devolop:
---
+++ dist/main.py
@@ -0,0 +1,26 @@
+import importlib
+import argparse
+from pyspark.sql import SparkSession
+from parse import parse
+
+
+def _parse_arguments():
+    """Parse arguments provided by spark-submit command"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--job", required=True)
+    return parser.parse_args()
+
+
+def main():
+    args = _parse_arguments()
+    """ Main function executed by spark submit command"""
+    configs_module = importlib.import_module(f"shared.config")
+    configs = configs_module.get_configs("config.ini", "movies_app")
+    spark = SparkSession.builder.appName(configs.get("app_name")).getOrCreate()
+
+    job_module = importlib.import_module(f"jobs.{args.job}")
+    job_module.run_job(spark, configs)
+
+
+if __name__ == "__main__":
+    main()
